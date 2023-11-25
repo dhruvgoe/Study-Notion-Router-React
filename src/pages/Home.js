e1a73fe42c822bc1d189c3133715393d6a911b91
dhruvgoel1234 <02619011621.aiml@gmail.com>
2023-11-25 23:15:21 +0530

    Home Section Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Home = ({isLoggedIn}) => {
+const Home = ({ isLoggedIn }) => {
   return (
-    <div className='flex justify-center items-center text-white text-3xl h-full'>
+    <div className="flex justify-center items-center text-white text-3xl h-full">
       Home
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
